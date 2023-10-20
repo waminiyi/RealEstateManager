a423e121ab95738d36465f91aafed13997984134
Patrick SOTO <mr.patricksoto@gmail.com>
2023-10-20 18:19:54 +0200

    Network monitoring function created and tested

---
+++ app/src/androidTest/java/com/waminiyi/realestatemanager/util/REMAndroidTestRunner.kt
@@ -0,0 +1,17 @@
+package com.waminiyi.realestatemanager.util
+
+import android.app.Application
+import android.content.Context
+import androidx.test.runner.AndroidJUnitRunner
+import dagger.hilt.android.testing.HiltTestApplication
+
+class REMAndroidTestRunner : AndroidJUnitRunner() {
+    override fun newApplication(
+        cl: ClassLoader,
+        appName: String,
+        context: Context
+    ) : Application {
+        return super.newApplication(
+            cl, HiltTestApplication::class.java.name, context)
+    }
+}
---
+++ app/src/main/java/com/waminiyi/realestatemanager/RemApplication.kt
@@ -0,0 +1,8 @@
+package com.waminiyi.realestatemanager
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class RemApplication : Application() {
+}
