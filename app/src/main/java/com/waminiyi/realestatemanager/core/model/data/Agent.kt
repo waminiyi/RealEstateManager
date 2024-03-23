fd67800ebe0450184042b012ef1b2ed39a2d0b96
PatouSoto <patrick.soto@diabeloop.fr>
2024-03-23 20:41:21 +0100

    Kdoc added to Agent class

--- app/src/main/java/com/waminiyi/realestatemanager/core/model/data/Agent.kt
+++ app/src/main/java/com/waminiyi/realestatemanager/core/model/data/Agent.kt
@@ -1,5 +1,15 @@
 package com.waminiyi.realestatemanager.core.model.data
 
+/**
+ * Data class representing an agent.
+ * @property uuid The unique identifier of the agent.
+ * @property firstName The first name of the agent.
+ * @property lastName The last name of the agent.
+ * @property email The email address of the agent.
+ * @property phoneNumber The phone number of the agent.
+ * @property photoUrl The URL to the photo of the agent.
+ * @property roles The roles assigned to the agent. By default, the agent is assigned the role 'AGENT'.
+ */
 data class Agent(
     val uuid: String,
     val firstName: String,
@@ -10,6 +20,10 @@ data class Agent(
     val roles: Set<Role> = setOf(Role.AGENT)
 )
 
+/**
+ * Enumeration class representing roles.
+ * Roles can be ADMIN or AGENT.
+ */
 enum class Role {
     ADMIN,
     AGENT
