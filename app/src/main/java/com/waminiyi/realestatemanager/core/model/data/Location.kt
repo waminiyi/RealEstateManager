a2f7dea7ff6b8bd986c580f1b9fd1a049d5c1824
PatouSoto <patrick.soto@diabeloop.fr>
2024-03-23 22:09:35 +0100

    Kdoc added to Filter class

--- app/src/main/java/com/waminiyi/realestatemanager/core/model/data/Location.kt
+++ app/src/main/java/com/waminiyi/realestatemanager/core/model/data/Location.kt
@@ -1,5 +1,10 @@
 package com.waminiyi.realestatemanager.core.model.data
 
+/**
+ * Data class representing a location with latitude and longitude coordinates.
+ * @property latitude The latitude coordinate of the location.
+ * @property longitude The longitude coordinate of the location.
+ */
 data class Location(
     val latitude: Double,
     val longitude: Double
